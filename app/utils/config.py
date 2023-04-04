3186382a5f1e6423bc908e64c48895ee3716ef64
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-04-04 15:04:31 +0200

    Created config.py file

---
+++ app/utils/config.py
@@ -0,0 +1,8 @@
+import yaml
+from typing import Dict
+
+from app.settings import ROOT_DIR
+
+def get_config(path) -> Dict:
+    with open(ROOT_DIR + path, "r") as config_file:
+        return yaml.safe_load(config_file)
