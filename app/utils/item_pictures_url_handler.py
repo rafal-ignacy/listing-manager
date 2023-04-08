a3bfdeb4fa532a72a706db306b04378f22ed1881
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-04-08 17:29:13 +0200

    Created item_pictures_url_handler.py - whole class

---
+++ app/utils/item_pictures_url_handler.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from string import Template
+from typing import List
+import requests
+
+from app.utils.config import get_config
+from app.exceptions import NoPicturesOnHosting, HostingCommunicationError
+
+
+@dataclass
+class ItemPicturesUrlHandler:
+    def __init__(self, inventory_id: int, type: str) -> None:
+        self.inventory_id: int = inventory_id
+        self.type: str = type
+        self.url_template: Template = Template(get_config("app/data/config.yaml")["item_pictures_url_handler"]["url_template"])
+
+    def get_urls(self) -> str:
+        picture_index: int = 1
+        picture_id_prefix: str = str(self.inventory_id) + self.type + "-"
+        pictures_urls_list: List = []
+        while True:
+            picture_id: str = picture_id_prefix + str(picture_index)
+            url: str = self.url_template.substitute(picture_id=picture_id)
+            try:
+                request: requests.Response = requests.get(url)
+            except:
+                raise HostingCommunicationError
+            if request.status_code != 200:
+                break
+            pictures_urls_list.append(url)
+            picture_index += 1
+        if len(pictures_urls_list) == 0:
+            raise NoPicturesOnHosting
+        pictures_urls_str: str = ";".join(pictures_urls_list)
+        return pictures_urls_str
