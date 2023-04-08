568d4962eb2dda1c5aa7d3bf19d49deaa6d671c1
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-04-08 17:27:49 +0200

    Refactored config.py file

--- app/utils/config.py
+++ app/utils/config.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 from app.settings import ROOT_DIR
 
+
 def get_config(path) -> Dict:
     with open(ROOT_DIR + path, "r") as config_file:
         return yaml.safe_load(config_file)
