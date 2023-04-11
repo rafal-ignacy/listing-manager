a797137f7b2860bd30936a07380a33daa788801c
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-04-11 22:31:21 +0200

    Created item objects creator file

---
+++ app/item/item_creator.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from app.data_models.item import Item
+from app.utils.item_pictures_url_handler import ItemPicturesUrlHandler
+from app.exceptions import ItemObjectNotCreated
+
+
+@dataclass
+class ItemCreator:
+    def create_item(self, inventory_id: int, type: str, price: float, dimensions: str) -> Item:
+        item_pictures_url_handler: ItemPicturesUrlHandler = ItemPicturesUrlHandler(inventory_id, type)
+        try:
+            pictures_url: str = item_pictures_url_handler.get_urls()
+        except Exception as error:  # noqa: E722
+            raise ItemObjectNotCreated(error)
+        return Item(inventory_id, type, price, dimensions, pictures_url)
