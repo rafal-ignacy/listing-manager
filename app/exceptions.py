7f7d825afa3c4ac8a03831fafe9f3824c41b9feb
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-04-11 22:28:09 +0200

    Added new exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,5 +1,18 @@
 class NoPicturesOnHosting(Exception):
     pass
 
+
 class HostingCommunicationError(Exception):
+    pass
+
+
+class ItemObjectNotCreated(Exception):
+    pass
+
+
+class DatabaseConnectionError(Exception):
+    pass
+
+
+class DatabaseAddItemError(Exception):
     pass
