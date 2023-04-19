5d6f109f30630a29b6982710dcbd406eac050fb3
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-04-19 22:58:24 +0200

    Added new function in config

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,9 +1,15 @@
 import yaml
+import json
 from typing import Dict
 
 from app.settings import ROOT_DIR
 
 
-def get_config(path) -> Dict:
+def get_yaml_config(path) -> Dict:
     with open(ROOT_DIR + path, "r") as config_file:
         return yaml.safe_load(config_file)
+
+
+def get_json_config(path) -> Dict:
+    with open(ROOT_DIR + path, "r") as config_file:
+        return json.loads(config_file.read())
