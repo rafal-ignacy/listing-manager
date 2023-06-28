df46816b83244e3a0de0895071f17085871d4670
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-06-28 20:03:52 +0200

    Created JSON template class

---
+++ app/utils/json_template.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Dict
+import json
+
+
+@dataclass
+class JsonTemplate:
+    def __init__(self, dictionary: Dict) -> None:
+        self.dictionary: str = json.dumps(dictionary)
+
+    def generate(self, variables: Dict) -> Dict:
+        for key, value in variables.items():
+            if isinstance(value, str) or isinstance(value, int) or isinstance(value, float):
+                self.dictionary = self.dictionary.replace("{{ " + key + " }}", str(value))
+            elif isinstance(value, list):
+                list_string: str = ""
+                for element in value:
+                    list_string += "\"" + element + "\", "
+                print("[[ \"{{" + key + "}}\" ]]")
+                self.dictionary = self.dictionary.replace("[[\"{{ " + key + " }}\"]]", "[" + list_string[:-2] + "]")
+            else:
+                raise TypeError("Passed incorrect variable type!")
+        return json.loads(self.dictionary)
