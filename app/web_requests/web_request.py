14cdfe12652c376d51f7e5c6d8079e9b9305454e
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-06-28 20:05:32 +0200

    Modified web_request file - different request types

--- app/web_requests/web_request.py
+++ app/web_requests/web_request.py
@@ -11,19 +11,27 @@ class WebRequest:
     headers: Optional[Dict] = None
     payload: Optional[Dict] = None
 
-    def response(self):
+    def execute(self, method_type: str) -> Dict | bytes | None:
         try:
-            if self.payload is not None:
-                request = requests.post(self.url, headers=self.headers, data=self.payload, timeout=10)
-            else:
-                request = requests.get(self.url, headers=self.headers, timeout=10)
+            match method_type:
+                case "POST":
+                    request = requests.post(self.url, headers=self.headers, json=self.payload, timeout=10)
+                case "PUT":
+                    request = requests.put(self.url, headers=self.headers, json=self.payload, timeout=10)
+                case "GET":
+                    request = requests.get(self.url, headers=self.headers, data=self.payload, timeout=10)
+                case "DELETE":
+                    request = requests.delete(self.url, headers=self.headers, data=self.payload, timeout=10)
         except Exception as error:
-            raise RequestError(error)
+            raise RequestError(method_type + " " + str(error))
 
         if request.ok:
             if request.headers.get("Content-Type") == "application/json" or request.headers.get("Content-Type") == "application/json; charset=utf-8":
-                return request.json()
+                try:
+                    return request.json()
+                except Exception:
+                    return None
             else:
                 return request.content
         else:
-            raise RequestNotCorrectError(request.status_code + " " + request.text)
+            raise RequestNotCorrectError(str(request.status_code) + " " + request.text)
