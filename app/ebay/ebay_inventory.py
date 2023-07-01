13a64a001bdd1c0b26cedb92167e3c0479cc1e72
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-07-01 11:52:32 +0200

    Added create_offer method in ebay_inventory.py

--- app/ebay/ebay_inventory.py
+++ app/ebay/ebay_inventory.py
@@ -3,6 +3,8 @@ from typing import List
 
 from app.web_requests.web_requests import WebRequests
 from app.ebay.ebay_auth import EbayAuth
+from app.utils.listing_description_handler import ListingDescriptionHandler
+from app.utils.price_exchange_handler import PriceExchangeHandler
 
 
 @dataclass
@@ -14,3 +16,11 @@ class EbayInventory:
     def create_inventory_item(self, platform_id: str, inventory_id: int, type: str, picture_urls: List):
         access_token: str = self.ebay_auth.get_access_token()
         self.web_requests.ebay_create_inventory_item(access_token, platform_id, inventory_id, type, picture_urls)
+
+    def create_offer(self, platform_id: str, inventory_id: int, type: str, price: float, dimensions: str):
+        access_token: str = self.ebay_auth.get_access_token()
+        listing_description_handler: ListingDescriptionHandler = ListingDescriptionHandler()
+        listing_description: str = listing_description_handler.create_description(platform_id, type, dimensions)
+        price_exchange_handler: PriceExchangeHandler = PriceExchangeHandler()
+        price = price_exchange_handler.get_price(price, platform_id)
+        self.web_requests.ebay_create_offer(access_token, platform_id, inventory_id, type, listing_description, price)
