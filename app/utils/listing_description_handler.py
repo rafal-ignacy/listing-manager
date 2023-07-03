23d9e296a9393928e27d3bd9242e3064e5ccd3b0
rafal-ignacy <rafal.ignacy.99@gmail.com>
2023-07-03 22:15:20 +0200

    Fixed old and added new functions in listing_description_handler.py

--- app/utils/listing_description_handler.py
+++ app/utils/listing_description_handler.py
@@ -7,12 +7,14 @@ from app.settings import ROOT_DIR
 @dataclass
 class ListingDescriptionHandler:
     def create_description(self, platform_id: str, item_type: str, dimensions: str):
-        description_template: str = self.get_description_template(platform_id, item_type)
+        self.__description: str = self.get_description_template(platform_id, item_type)
         dimensions_cm = self.prepare_dimensions_cm(dimensions)
         dimensions_inch = self.prepare_dimensions_inch(dimensions)
-        description: str = self.replace_dimensions(description_template, dimensions_cm, "cm")
-        description = self.replace_dimensions(description, dimensions_inch, "inch")
-        return description
+        self.replace_dimensions(dimensions_cm, "cm")
+        self.replace_dimensions(dimensions_inch, "inch")
+        self.prepare_final_formatting()
+        print(self.__description)
+        return self.__description
 
     def get_description_template(self, platform_id: str, item_type: str) -> str:
         match platform_id:
@@ -27,9 +29,8 @@ class ListingDescriptionHandler:
             return description_file.read()
 
     def prepare_dimensions_cm(self, dimensions: str) -> List[str]:
-        dimensions = dimensions.replace(",", ".")
+        dimensions = dimensions.replace(".", ",")
         dimensions_cm: List = dimensions.split(";")
-        dimensions_cm = [dimension.replace(".", ",") for dimension in dimensions_cm]
         return dimensions_cm
 
     def prepare_dimensions_inch(self, dimensions: str) -> List[str]:
@@ -39,10 +40,21 @@ class ListingDescriptionHandler:
         dimensions_inch = [dimension if dimension[-1] != "0" else dimension[:-2] for dimension in dimensions_inch]
         return dimensions_inch
 
-    def replace_dimensions(self, description: str, dimensions_list: List, dimension_unit: str) -> str:
+    def replace_dimensions(self, dimensions_list: List, dimension_unit: str) -> None:
         index = 1
         for dimension in dimensions_list:
             dimension_replace_id = "{{ dimension_" + str(dimension_unit) + "_" + str(index) + " }}"
-            description = description.replace(dimension_replace_id, dimension)
+            self.__description = self.__description.replace(dimension_replace_id, dimension)
             index += 1
-        return description
+
+    def prepare_final_formatting(self) -> None:
+        self.__description = self.__description.replace("\n", "")
+        self.__description = self.__description.replace("\r", "")
+
+        loop_index = 0
+        for _ in range(len(self.__description)):
+            if self.__description[loop_index] == "\"":
+                self.__description = self.__description[:loop_index] + "\\" + self.__description[loop_index:]
+                loop_index += 2
+            else:
+                loop_index += 1
